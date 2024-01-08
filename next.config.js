fdf53f13a0bdf04196c7abbc0ae462ddbf4f6c39
Jorge Monge <jlo4321jl@gmail.com>
2024-01-08 14:15:47 -0600

    🔧 Add images urls

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	  images: {
+	domains: ['wallpaperaccess.com', 'books.google.com'],
+  },
+}
 
 module.exports = nextConfig
